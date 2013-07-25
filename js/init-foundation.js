d655aff8ec3bde521233058dc7aa8bbfa75d31a4
Arturas Lebedevas <nsmeta@gmail.com>
2013-07-25 12:02:47 +0100

    oops!

--- js/init-foundation.js
+++ js/init-foundation.js
@@ -1,14 +1,5 @@
 /* Ugly hack :-( */
 
 Template._init_foundation.rendered = function () {
-    console.log('[smart-package] template rendered');
     $(document).foundation();
 };
-
-Meteor.startup(function () {
-    console.log('[smart package] Meteor.startup');
-});
-
-$(document).ready(function () {
-    console.log('[smart-package] document.ready');
-});
